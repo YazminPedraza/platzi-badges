6bbca91acffe52ed0e2559d32bb1a7f9566e247d
YazminPedraza <yazminpedraza.it@gmail.com>
2019-11-18 09:09:38 -0600

    How add styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,23 +1,29 @@
 import React from 'react';
+
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
     render(){
-        return <div>
-            <div>
-                <img src={confLogo} alt="Conference Logo"/>
-            </div>
+        return (
+            <div className="Badge">
+                <div className= "Badge__header">
+                    <img src={confLogo} alt="Conference Logo"/>
+                </div>
 
-            <div>
-                <img src="" alt="Avatar"/>
-                <h1>Philippa <br/> Georgiou</h1>
-            </div>
+                <div className= "Badge__section-name">
+                    <img className= "Badge__avatar" src="https://cudebi.files.wordpress.com/2018/01/image0025.png" alt="Avatar"/>
+                    <h1>Philippa <br/> Georgiou</h1>
+                </div>
 
-            <div>
-                <p>USS Shenzhou Captain</p>
-                <p>@CaptainGeorgiou</p>
+                <div className="Badge__section-info">
+                    <h3>USS Shenzhou Captain</h3>
+                    <div>@CaptainGeorgiou</div>
+                </div>
+
+                <div className="Badge__footer">#platziconf</div>
             </div>
-        </div>
+        )
     }
 }
 
