4512f6198630b29c6b9e5caebb4fff24c6ff5fd4
YazminPedraza <yazminpedraza.it@gmail.com>
2019-11-17 19:37:53 -0600

    ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; //Análogo al create element
+//Si queremos escribir JSX siempre tenemos que importar React from react
+import ReactDOM from 'react-dom'; //Análogo a append appendChild
 
 const element = <h1>Hello, Platzi Badges!</h1>;
 
