ce59a0f8400395b3e62429594a1b4a7ac63fb830
YazminPedraza <yazminpedraza.it@gmail.com>
2019-11-18 21:20:58 -0600

    Events and handleEvent

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,7 +3,8 @@ import React from 'react';
 import './styles/BadgeNew.css';
 import header from '../images/badge-header.svg';
 import Navbar from "../components/Navbar";
-import Badge from "../components/Badge"
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
     render() {
@@ -12,13 +13,14 @@ class BadgeNew extends React.Component {
                 <Navbar />
                 <div className="BadgeNew__hero">
                     <img className="img-fluid" src={header} alt="Logo"/>
-
                 </div>
-
                 <div className="container">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-6">
                             <Badge firstName="Philippa" lastName="Georgiou" jobTitle="USS Shenzhou Captain" twitter="captainGeorgiou" avatar="https://cudebi.files.wordpress.com/2018/01/image0025.png"/>   
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm/>
                         </div>
                     </div>
                 </div>
