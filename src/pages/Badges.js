ddfcb02901c1e2da7e4ac4ebdd48e9eb675f99b7
YazminPedraza <yazminpedraza.it@gmail.com>
2020-01-05 00:14:25 -0600

    Polling (automatic updates)

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -6,6 +6,7 @@ import confLogo from "../images/badge-header.svg";
 import BadgesList from "../components/BadgesList";
 import PageLoading from "../components/PageLoading";
 import PageError from "../components/PageError";
+import MiniLoader from "../components/MiniLoader";
 
 import api from "../api";
 
@@ -18,6 +19,11 @@ class Badges extends Component {
 
   componentDidMount(){
     this.fetchData();
+    this.intervalId = setInterval(this.fetchData, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
 
   fetchData = async() => {
@@ -32,7 +38,7 @@ class Badges extends Component {
   }
 
   render() {
-    if (this.state.loading === true) {
+    if (this.state.loading === true && !this.state.data) {
       return <PageLoading />;
     }
 
@@ -62,6 +68,7 @@ class Badges extends Component {
           </div>
 
           <BadgesList badges={this.state.data} />
+          {this.state.loading && <MiniLoader />}
         </div>
       </React.Fragment>
     );
