5e05ab9750481e1afc485431cb81d174edc219df
YazminPedraza <yazminpedraza.it@gmail.com>
2020-01-19 11:08:17 -0600

    Search filter with useMemo and custom hook

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,59 +1,112 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Gravatar from './Gravatar';
+import React, { Component, useState } from "react";
+import { Link } from "react-router-dom";
+import Gravatar from "./Gravatar";
 
-import './styles/BadgesList.css';
+import "./styles/BadgesList.css";
 
 class BadgeListItem extends Component {
-    render(){
-        return (
-            <div className="BadgesListItem">
-                <Gravatar className="BadgesListItem__avatar" email={this.props.badge.email} alt={`${this.props.badge.firstName}''${this.props.badge.lastName}`}/>
-                {/* <img className="BadgesListItem__avatar" src={this.props.badge.avatarUrl} alt={`${this.props.badge.firstName}''${this.props.badge.lastName}`}
+  render() {
+    return (
+      <div className="BadgesListItem">
+        <Gravatar
+          className="BadgesListItem__avatar"
+          email={this.props.badge.email}
+          alt={`${this.props.badge.firstName}''${this.props.badge.lastName}`}
+        />
+        {/* <img className="BadgesListItem__avatar" src={this.props.badge.avatarUrl} alt={`${this.props.badge.firstName}''${this.props.badge.lastName}`}
                 /> */}
 
-                <div className="BadgesListItem__info">
-                    <strong>
-                        {this.props.badge.firstName} {this.props.badge.lastName}
-                    </strong>
-                        <br/>
-                        <img className="twitter-logo" src="https://img.icons8.com/color/48/000000/twitter.png"/>
-                        @{this.props.badge.twitter}
-                        <br/>
-                        {this.props.badge.jobTitle}
-                </div>
-            </div>
-        );
-    }
+        <div className="BadgesListItem__info">
+          <strong>
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </strong>
+          <br />
+          <img
+            className="twitter-logo"
+            src="https://img.icons8.com/color/48/000000/twitter.png"
+          />
+          @{this.props.badge.twitter}
+          <br />
+          {this.props.badge.jobTitle}
+        </div>
+      </div>
+    );
+  }
 }
 
+/* Custom hook declaration (Hook used on BadgesList)*/
 
-class BadgesList extends Component {
-    render() {
-        if(this.props.badges.length === 0) {
-            return (
-                <div>
-                    <h3>No badges were found</h3>
-                    <Link className="btn btn-primary" to="/badges/new">Create new badge</Link>
-                </div>
-            );
-        }
-        return (
-            <div className="BadgesList">
-                <ul>
-                    {this.props.badges.map(badge => {
-                        return (
-                            <li key={badge.id}>
-                                <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}`}>
-                                    <BadgeListItem badge={badge}/>
-                                </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState("");
+  const [filteredBadges, setFilteredBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const result = badges.filter(badge => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilteredBadges(result);
+  }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
+function BadgesList(props) {
+  const badges = props.badges;
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+  if (filteredBadges.length === 0) {
+    return (
+      <div>
+        <div className="form-group">
+          <label htmlFor="">Filter Badges</label>
+          <input
+            type="text"
+            className="form-control"
+            value={query}
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+        <h3>No badges were found</h3>
+        <Link className="btn btn-primary" to="/badges/new">
+          Create new badge
+        </Link>
+      </div>
+    );
+  }
+  return (
+    <div className="BadgesList">
+      <div className="form-group">
+        <label htmlFor="">Filter Badges</label>
+        <input
+          type="text"
+          className="form-control"
+          value={query}
+          onChange={e => {
+            setQuery(e.target.value);
+          }}
+        />
+      </div>
+      <ul className="list-unstyled">
+        {filteredBadges.map(badge => {
+          return (
+            <li key={badge.id}>
+              <Link
+                className="text-reset text-decoration-none"
+                to={`/badges/${badge.id}`}
+              >
+                <BadgeListItem badge={badge} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default BadgesList;
